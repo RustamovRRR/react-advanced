fee86832d311ffc45d75e19d1d065ff106f6b69e
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-08 22:34:58 +0400

    useState - Basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("hello world"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState("Peter Parker");
+  const handleTitle = () => {
+    if (text === "Peter Parker"){
+
+      setText("Hello Peter");
+    } else{
+      
+      setText("Peter Parker");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleTitle}>
+        click me
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;

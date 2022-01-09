2acf7fa43733c50ae5587cf34504efb95e6ea02a
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-09 19:03:10 +0400

    useState object example with using spread operator

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "walter",
+    age: 73,
+    message: "random message",
+  });
+
+  // using spread operator in object
+  const changeMessage = () => {
+    setPerson({ ...person, message: "hello Astrid" });
+  };
+
+  return (
+    <>
+      <h1>{person.name}</h1>
+      <h1>{person.age}</h1>
+      <h1>{person.message}</h1>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
